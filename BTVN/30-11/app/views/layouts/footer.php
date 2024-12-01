4844fa5b1c7b2098a81fb2125a8577be3951700e
Lê Doãn Tú <137909137+ldoantu04@users.noreply.github.com>
2024-12-01 23:23:44 +0700

    add btvn 30/11 product mvc

---
+++ BTVN/30-11/app/views/layouts/footer.php
@@ -0,0 +1,23 @@
+
+<style>
+    hr {
+        border: 2px solid #000000;
+    }
+
+    .footer {
+        text-align: center;
+        margin-top: 20px;
+        font-size: 30px;
+        font-weight: bold;
+    }
+</style>
+<div class="footer">
+    <hr>
+    TLU'S MUSIC GARDEN
+</div>
+
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</body>
+</html>
